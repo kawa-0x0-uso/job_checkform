0053a866633615b70067ae68d46b8db49dae7626
kawa-0x0-uso <96374232+kawa-0x0-uso@users.noreply.github.com>
2023-09-06 18:04:17 +0900

    ・htmlの要素を追加 ・サービスの部分だけ、子要素にチェックが入ったら親要素にもチェックが入る / 子要素のチェックが全て外れたら親要素のチェックが外れるように修正

--- js/index.js
+++ js/index.js
@@ -1,25 +1,17 @@
-// // ddのinput中カテ（業界）が変更された時
-// $(".preflist dd input[type='checkbox']").change(function () {
-//     var area = $(this).parents(".preflist").find("dt input[type='checkbox']");
-//     // 選択された中カテ（業界）を変数に格納
-//     if ($(this).is(":checked")) {
-//         // チェックがついた時
-//         area.prop("checked",true);
-//       // 大カテ（業種）をチェックする
-//     } else {
-//         area.prop("checked",false);
-//       // 選択した大カテ（業種）のチェックを外す
-//     }
-// });
-
-let othersService = document.getElementsByName('othersService');
+let othersService = document.getElementById('othersService');
 let category = document.getElementById('categoryOthersService');
 
-category.addEventListener('change',check);
+othersService.addEventListener('change',check);
 
 function check(){
-  for(let i = 0;i<othersService.length;i++){
-    othersService[i].checked = true;
-    console.log(othersService[i]);
+  let othersService_child = document.querySelectorAll('ul#othersService>li>input[type="checkbox"]:checked');
+
+  console.log(othersService_child.length);
+
+  if(othersService_child.length != 0){
+    category.checked = true;
+  }else{
+    category.checked = false;
   }
+  
 }
