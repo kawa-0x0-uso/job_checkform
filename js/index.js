f80d9bf45c07d2e2c9c53fb812bc5c567f42ed73
kawa-0x0-uso <96374232+kawa-0x0-uso@users.noreply.github.com>
2023-09-07 17:24:59 +0900

    ・HTMLの変更と追加 ・CSSで見た目を簡易的に調整 ・jsのコードを整理

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
-let othersService = document.getElementById('othersService');
-let category = document.getElementById('categoryOthersService');
+// カテゴリ名を配列でセットする
+let category = ["othersService","educationService"];
 
-othersService.addEventListener('change',check);
+// WEBページ上で変化があったら関数checkを呼び出す
+window.addEventListener('change',check);
 
+// チェックボックスを連動させる
 function check(){
-  let othersService_child = document.querySelectorAll('ul#othersService>li>input[type="checkbox"]:checked');
+  for(let i = 0;i<category.length;i++){
+    // 各業界を取得
+    let industry = document.getElementById(category[i]);
+    // 各業種を取得
+    let industries = document.getElementById("category_"+category[i]);
 
-  console.log(othersService_child.length);
+    // 各業界でチェックされている項目を取得
+    let checkChild = document.querySelectorAll('ul#'+ category[i] + '>li>input[type="checkbox"]:checked');
 
-  if(othersService_child.length != 0){
-    category.checked = true;
-  }else{
-    category.checked = false;
+    if(checkChild.length != 0){
+      // 各業界でチェックされている項目が0以上の場合、連動する業種のチェックを入れる
+      industries.checked = true;
+    }else{
+      // 各業界でチェックされている項目が0の場合、連動する業種のチェックを外す
+      industries.checked = false;
+    } 
   }
-  
 }
